8b80f6e8a69e00699428514e8309f10b9f0772cb
Fartita Jehela <varvaran96@yandex.ru>
2024-08-03 22:21:07 +0300

    insert toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,14 +39,14 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation (libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation(libs.material.v180)
+    implementation(libs.material.v190)
     implementation(libs.glide)
     annotationProcessor(libs.compiler)
 }
