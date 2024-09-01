20c48e652c3be9a2dacf12cf5f97ea199c46e122
Fartita Jehela <varvaran96@yandex.ru>
2024-09-01 14:37:02 +0300

    sprint 11 1st attempt

---
+++ app/src/main/java/com/example/playlistmaker/constants/SharedPreference.kt
@@ -0,0 +1,7 @@
+package com.example.playlistmaker.constants
+
+object SharedPreference {
+    const val PRACTICUM_PREFERENCES = "playlist_maker_preferences"
+    const val THEME_KEY = "theme_key"
+    const val TRACKS_KEY = "track_key"
+}
--- app/src/main/java/com/example/playlistmaker/recycler/SearchAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/recycler/SearchAdapter.kt
@@ -15,11 +15,16 @@ import com.example.playlistmaker.model.Track
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-class SearchAdapter(private val items: ArrayList<Track>) : RecyclerView.Adapter<SearchViewHolder>() {
+class SearchAdapter(private val items: ArrayList<Track>, val clickListener: TrackListener) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_recycler_search_layout, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_recycler_search_layout, parent, false)
         return SearchViewHolder(view)
+    }
+
+    fun interface TrackListener {
+        fun onTrackClick(track: Track)
     }
 
     override fun getItemCount(): Int {
@@ -28,27 +33,31 @@ class SearchAdapter(private val items: ArrayList<Track>) : RecyclerView.Adapter<
 
     override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
         holder.bind(items[position])
+        holder.itemView.setOnClickListener {
+            clickListener.onTrackClick(items[position])
+        }
     }
-}
 
-class SearchViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    private val trackName: TextView = itemView.findViewById(R.id.trackName)
-    private val artistName: TextView = itemView.findViewById(R.id.trackInfo)
-    private val avatar: ImageView = itemView.findViewById(R.id.imageTrack)
+        private val trackName: TextView = itemView.findViewById(R.id.trackName)
+        private val artistName: TextView = itemView.findViewById(R.id.trackInfo)
+        private val avatar: ImageView = itemView.findViewById(R.id.imageTrack)
 
 
-    fun bind(track: Track) {
-        trackName.text = track.trackName
-        val time = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTime).toString()
-        artistName.text = "${track.artistName} • ${time}"
+        fun bind(track: Track) {
+            trackName.text = track.trackName
+            val time =
+                SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTime).toString()
+            artistName.text = "${track.artistName} • ${time}"
 
-        Glide.with(itemView)
-            .load(track.artworkUrl100)
-            .centerCrop()
-            .placeholder(R.drawable.ic_track)
-            .transform(RoundedCorners(itemView.resources.getDimensionPixelSize(R.dimen.corner_radius_art)))
-            .into(avatar)
+            Glide.with(itemView)
+                .load(track.artworkUrl100)
+                .centerCrop()
+                .placeholder(R.drawable.ic_track)
+                .transform(RoundedCorners(itemView.resources.getDimensionPixelSize(R.dimen.corner_radius_art)))
+                .into(avatar)
 
+        }
     }
 }
