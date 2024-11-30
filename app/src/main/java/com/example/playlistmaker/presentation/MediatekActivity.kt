8e7dd49021aaa4488dace8b0857500c4974264ab
Fartita Jehela <varvaran96@yandex.ru>
2024-11-30 13:43:47 +0300

    18 sprint

--- app/src/main/java/com/example/playlistmaker/presentation/MediatekActivity.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/MediatekActivity.kt
@@ -1,6 +1,31 @@
 package com.example.playlistmaker.presentation
 
-import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivityMediatekBinding
+import com.example.playlistmaker.presentation.recycler.LibraryViewPageAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
-class MediatekActivity: Activity() {
+class MediatekActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityMediatekBinding
+    private lateinit var tabMediator: TabLayoutMediator
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMediatekBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.backToMainActivity.setOnClickListener { finish() }
+        binding.viewPager.adapter = LibraryViewPageAdapter(supportFragmentManager, lifecycle)
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = getString(R.string.favourites)
+                1 -> tab.text = getString(R.string.playlists)
+            }
+        }
+        tabMediator.attach()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
+    }
 }
---
+++ app/src/main/java/com/example/playlistmaker/presentation/viewmodels/library/PlayListViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.playlistmaker.presentation.viewmodels.library
+
+import androidx.lifecycle.ViewModel
+class PlayListViewModel: ViewModel() {
+}
