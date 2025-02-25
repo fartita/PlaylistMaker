ebed6a96918f6afe83f6768935570b8154ab3b43
Fartita Jehela <varvaran96@yandex.ru>
2025-02-25 20:15:37 +0300

    23 sprint

---
+++ app/src/main/java/com/example/playlistmaker/domain/playlist/PlaylistLibraryInteractorImpl.kt
@@ -0,0 +1,22 @@
+package com.example.playlistmaker.domain.playlist
+
+import android.content.Context
+import com.example.playlistmaker.R
+import com.example.playlistmaker.domain.model.Playlist
+import com.example.playlistmaker.domain.model.Track
+import com.example.playlistmaker.domain.repository.PlaylistRepository
+import kotlinx.coroutines.flow.Flow
+
+class PlaylistLibraryInteractorImpl(private val playlistRepository: PlaylistRepository) :
+    PlaylistLibraryInteractor {
+    override fun getPlayLists(): Flow<List<Playlist>> {
+        return playlistRepository.getPlayLists()
+    }
+    override suspend fun addTrack(context: Context, track: Track, playList: Playlist): String {
+        val tracks = playlistRepository.getTrackList(playList.id)
+        return if (tracks.isEmpty() || !tracks.contains(track)) {
+            playlistRepository.addTrack(track, playList)
+            context.resources.getString(R.string.add_playlist)
+        } else context.resources.getString(R.string.track_in_playlist)
+    }
+}
