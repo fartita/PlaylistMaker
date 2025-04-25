36c3c97d06bfa0f4ee0957aee63cd10652096573
Fartita Jehela <varvaran96@yandex.ru>
2025-04-25 18:34:36 +0300

    25 sprint

--- app/src/main/java/com/example/playlistmaker/presentation/viewmodels/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/playlistmaker/presentation/viewmodels/player/PlayerViewModel.kt
@@ -7,29 +7,27 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.playlistmaker.data.states.PlayerState
-import com.example.playlistmaker.domain.PlayControlInteractor
 import com.example.playlistmaker.domain.api.HistoryInteractor
 import com.example.playlistmaker.domain.db.FavouriteTracksInteractor
 import com.example.playlistmaker.domain.model.Playlist
 import com.example.playlistmaker.domain.model.Track
 import com.example.playlistmaker.domain.playlist.PlaylistLibraryInteractor
+import com.example.playlistmaker.presentation.services.PlayerControl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-class PlayerViewModel(private val interactor: PlayControlInteractor,
+class PlayerViewModel(
                       private val historyInteractor: HistoryInteractor,
                       private val favoriteInteractor: FavouriteTracksInteractor,
                       private val playlistLibraryInteractor: PlaylistLibraryInteractor): ViewModel(){
 
     private val DELAY_MILLIS = 300L
 
-    private val stateLiveData = MutableLiveData(PlayerState.PREPARED)
+    private val stateLiveData = MutableLiveData<PlayerState>(PlayerState.Default())
     fun observeState(): LiveData<PlayerState> = stateLiveData
-
-    private val stateProgressTimeLiveData = MutableLiveData<String>()
-    fun observeTime(): LiveData<String> = stateProgressTimeLiveData
+    private var audioPlayerControl: PlayerControl? = null
 
     private val stateFavouriteData = MutableLiveData<Boolean>()
     fun observeFavouriteState(): LiveData<Boolean> = stateFavouriteData
@@ -40,39 +38,30 @@ class PlayerViewModel(private val interactor: PlayControlInteractor,
     private val addLiveData = MutableLiveData<String>()
     fun observeAddDtate(): LiveData<String> = addLiveData
 
-    private var timerJob: Job? = null
-
-
-    private fun startTimer(state: PlayerState) {
-        timerJob = viewModelScope.launch(Dispatchers.Default) {
-            while (state == PlayerState.PLAYING){
-                delay(DELAY_MILLIS)
-                stateProgressTimeLiveData.postValue( interactor.getProgressTime())
-            }
-        }
-    }
-
-    private fun cancelTimer() {
-        timerJob?.cancel()
-        timerJob = null
-    }
-
-
-    init {
-        interactor.setStateOnChangeListener { state->
-            stateLiveData.postValue(state)
-            val progressTime = interactor.getProgressTime()
-            stateProgressTimeLiveData.postValue(progressTime)
-            if (state == PlayerState.PREPARED) cancelTimer()
-        }
-    }
 
     fun getTrack(): Track {
         return historyInteractor.getTrack()
     }
 
+    fun setAudioPlayerControl(audioPlayerControl: PlayerControl) {
+        this.audioPlayerControl = audioPlayerControl
+        viewModelScope.launch {
+            audioPlayerControl.getPlayerState().collect {
+                stateLiveData.postValue(it)
+            }
+        }
+
+    }
+
+    fun showNotification(){
+        audioPlayerControl?.showNotification()
+    }
+
+    fun hideNotification(){
+        audioPlayerControl?.hideNotification()
+    }
+
     fun prepare(track: Track) {
-        interactor.preparePlayer(track.previewUrl)
         viewModelScope.launch {
             favoriteInteractor.getTrack(track.trackId).collect{ it ->
                 if(it != null){
@@ -85,22 +74,21 @@ class PlayerViewModel(private val interactor: PlayControlInteractor,
         }
     }
 
-    fun playbackControl() {
-        val state = interactor.playbackControl()
-        renderState(state)
-        if (state == PlayerState.PLAYING) startTimer(state) else cancelTimer()
+    fun onPlayerButtonClicked() {
+        if (stateLiveData.value is PlayerState.Playing) {
+            audioPlayerControl?.pausePlayer()
+        } else {
+            audioPlayerControl?.startPlayer()
+        }
     }
+
 
 
     override fun onCleared() {
         super.onCleared()
-        interactor.release()
+        audioPlayerControl = null
     }
 
-    fun onPause() {
-        interactor.pausePlayer()
-        renderState(PlayerState.PAUSED)
-    }
 
     fun onFavouriteClicked(track: Track) {
         viewModelScope.launch {
@@ -121,6 +109,7 @@ class PlayerViewModel(private val interactor: PlayControlInteractor,
             renderToastState(message)
         }
     }
+
     fun renderPlayLists() {
         viewModelScope.launch {
             playlistLibraryInteractor.getPlayLists()
@@ -130,7 +119,7 @@ class PlayerViewModel(private val interactor: PlayControlInteractor,
         }
     }
 
-    private fun renderState(state: PlayerState) {
-        stateLiveData.postValue(state)
+    fun removeAudioPlayerControl() {
+        audioPlayerControl = null
     }
 }
